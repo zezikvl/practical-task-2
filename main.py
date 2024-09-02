0a5ceedab6d6fb554396137d32f6a33510d90daf
zezikvl <zezik.vl@gmail.com>
2024-09-02 20:54:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+homework = '12'
+hours = '1.5'
+course_name = 'Python'
+time = '0.125'
+
+print('Курс:', course_name , 'всего задач:', homework, 'затрачено часов:', hours, 'среднее время выполнения', time, 'часа.')
